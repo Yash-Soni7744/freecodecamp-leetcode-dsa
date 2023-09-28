9b4d57c22b4a1ce859e0d30bf55fde014b37f5ff
Yash Soni <120086351+Yash-Soni7744@users.noreply.github.com>
2023-09-28 22:30:23 +0530

    I am GOAT

--- Caesars-Cipher.js
+++ Caesars-Cipher.js
@@ -1,33 +1,43 @@
 function rot13(str) {
-    
-    let space = []
-    for (let i=0; i<str.length; i++){
-        if (str[i]==" "){
-            space.push(i)
-        }
+  let space = []
+  for (let i=0; i<str.length; i++){
+    if (str[i]==" "){
+      space.push(i)
     }
-    let newStr = str.replace(/\W/g,'')
-    console.log(space)
+  }
+  let newStr = str.replace(/\s/g,'')
+  console.log(space)
+  console.log(newStr)
     
-    let up = []
-    for(let i in newStr){
+  let up = []
+    
+  var charCode = 0;
+  for(let i in newStr){
+    
+    if(newStr[i].match(/[A-Z]/g)){
+      charCode = newStr[i].codePointAt(0)
+      charCode += 13
         
-        var charCode = newStr[i].codePointAt(0)
-        charCode += 13
-        // console.log(charCode)
-        if(charCode > 90){
-            charCode = charCode - 26
-        }
-        // console.log(charCode)
-    up.push(String.fromCodePoint(charCode))
-    
+      if(charCode > 90){
+        charCode = charCode - 26
+      }
+      up.push(String.fromCodePoint(charCode))
+      // console.log(up)
     }
-
-    console.log(up)
-    for (let i in space){
-      up.splice(space[i],0," ")
+    else{
+      
+    up.push(newStr[i])
     }
-      return up.join('')
+    // console.log(up)
+  }
+  for (let i in space){
+    up.splice(space[i],0," ")
+  }
+  return up.join('')
+      // return up.join('')
+  // console.log(charCode)
+  // console.log(charCode)
 }
 
-rot13("SERR PBQR PNZC");
+
+rot13("SERR PBQR PNZC!");
