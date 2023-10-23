59bacb9e8c1dbb65fb4379d69957e12a53c52264
Yash Soni <120086351+Yash-Soni7744@users.noreply.github.com>
2023-10-23 20:58:50 +0530

    Create Method-overriding.js

---
+++ Method-overriding.js
@@ -0,0 +1,28 @@
+class Employee{
+    login(name){
+        console.log(name+' has logged in')
+    }
+    logout(name){
+        console.log(name+' has logged out')
+    }
+    requestLeaves(leaves){
+        console.log(`Employee has requested ${leaves} leaves - Auto approved`)
+    }
+}
+
+class Programmer extends Employee{
+    requestLeaves(leaves){
+        super.requestLeaves(4)
+        console.log('One extra is granted')
+    }
+}
+
+let e = new Programmer()
+e.login('rohan')
+e.logout('rohan')
+e.requestLeaves(3)
+
+//Here, console.log is Employee has requested 4 leaves \n One extra is granted
+//It overrides the Employee class Method of requestLeaves()
+
+//and, super is used to use all the Employee's
