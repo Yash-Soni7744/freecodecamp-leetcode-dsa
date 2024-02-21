e900c99a8893a11899fc3c08f8298a70fb899209
Yash Soni <120086351+Yash-Soni7744@users.noreply.github.com>
2024-02-21 11:47:32 +0530

    Create cp.js

---
+++ cp.js
@@ -0,0 +1,26 @@
+// function palin(str){
+//     str = str.replace(" ","")
+    
+//     for(let i=0; i<str.length; i++){
+//         if(str[i]!=str[str.length - (i+1)]){
+//             console.log('not palindrome')
+//             return false
+//         }
+//     }
+//     console.log("is palindrome")
+//     // return true
+// }
+// palin("race car")
+// palin("nitin")
+// palin("aman")
+
+let a = "i am boy a boy"
+let c = a.split(' ')
+
+for(let i in c){
+    let b = []
+    b.push(c[i])
+    for (let j in c){
+        if
+    }
+}
